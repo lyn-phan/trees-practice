52f7bcc640ec115eefe54a6afe0ca0a3b2bda4e8
Lynda Phan <lynda.kim.phan@gmail.com>
2021-02-24 13:20:23 -0800

    added more practice problems

--- binarytree.py
+++ binarytree.py
@@ -1,4 +1,4 @@
-#Implement a Binary Tree data structure
+# Implement a Binary Tree data structure
 
 class Binary_Node():
 
@@ -6,14 +6,102 @@ class Binary_Node():
         self.data = data
         self.left = left
         self.right = right
+[3:03 PM]
 
 class Binary_Tree():
 
     def __init__(self, root=None):
+        self.root = root
 
-node15 = Binary_Node(15)
-node9 = Binary_Node(9)
-node38 = Binary_Node(30)
-node8 = Binary_Node(8)
+    def add_node(self, node):
+        # first check to see if the root of our tree is empty
+        if self.root == None:
+            self.root = node
+            return
+        else:
+            def check_children(parent=self.root):
+                if not parent.left:
+                    parent.left = node
+                    return
+                if not parent.right:
+                    parent.right = node
+                    return
+                check_children(parent.left)
+            check_children()
 
-btree = Binary_Tree(node15)
+    def print_children(self, current=None):
+        if not current:
+            current = self.root
+        if current:
+            print(current.data)
+            if current.left:
+                self.print_children(current.left)
+            if current.right:
+                self.print_children(current.right)
+
+    # Replace a node
+    def replace_node(self, node_to_replace, new_node):
+
+        nodes_to_visit = [self.root]
+        # current = self.root
+        # [9]
+        while nodes_to_visit:
+            current = nodes_to_visit.pop(0)  # 9
+            if current.data == node_to_replace.data:  # 9 == 8
+                node_to_replace.data = new_node.data
+                return
+            else:
+                nodes_to_visit.extend([current.left, current.right])
+                print(nodes_to_visit)
+[3:03 PM]
+
+class Binary_Tree():
+
+    def __init__(self, root=None):
+        self.root = root
+
+    def add_node(self, node):
+        # first check to see if the root of our tree is empty
+        if self.root == None:
+            self.root = node
+            return
+        else:
+            def check_children(parent=self.root):
+                if not parent.left:
+                    parent.left = node
+                    return
+                if not parent.right:
+                    parent.right = node
+                    return
+                check_children(parent.left)
+            check_children()
+
+    def print_children(self, current=None):
+        if not current:
+            current = self.root
+        if current:
+            print(current.data)
+            if current.left:
+                self.print_children(current.left)
+            if current.right:
+                self.print_children(current.right)
+
+    # Replace a node
+    def replace_node(self, node_to_replace, new_node):
+
+        nodes_to_visit = [self.root]
+        # current = self.root
+        # [9]
+        while nodes_to_visit:
+            current = nodes_to_visit.pop(0)  # 9
+            if current.data == node_to_replace.data:  # 9 == 8
+                node_to_replace.data = new_node.data
+                return
+            else:
+                nodes_to_visit.extend([current.left, current.right])
+                print(nodes_to_visit)
+
+#which methods should we add? 
+#adding a new node to our tree
+#printing all of the tree nodes
+#replace a node 
